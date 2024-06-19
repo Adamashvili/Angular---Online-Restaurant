43f48032ed2e9a805c20c9d3ea5a23d570a4221b
irakli adamashvili <adamashvili.irakli8@gmail.com>
2024-06-19 16:48:54 +0400

    add post method

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -16,6 +16,9 @@ export class ProductsComponent implements OnInit {
   public categories: any;
   public foodList: any;
   public activeCategory: number = 0
+  public isPopuped: boolean = false
+  public itemQuantity!: string;
+  public dataToPost:any;
 
   showCategories() {
     this.service.getCategories().subscribe((items) => {
@@ -40,8 +43,37 @@ export class ProductsComponent implements OnInit {
   getFilteredData(filterData: any) {
     this.service.filterProducts(filterData.vegeterian, filterData.nuts, filterData.spiciness).subscribe(data => {
       this.foodList = data
+      this.activeCategory = -1
       
     })
     
+  }
+
+  openModal(item:any) {
+    this.isPopuped = true
+    this.dataToPost = item
+    
+    
+  }
+
+  addToCart(){
+    this.service.addToCart({
+      "quantity": this.itemQuantity,
+      "price": this.dataToPost.price,
+      "productId": this.dataToPost.id
+    }).subscribe({
+      next: () => {
+        alert("Product added to cart successfully")
+        this.isPopuped = false;
+      },
+      error: () => alert("Try again...")
+    })
+    
+    
+  }
+
+  closeModal(){
+    this.isPopuped = false
+    this.itemQuantity = "";
   }
 }
