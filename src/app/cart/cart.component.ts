50a8ae566679e57b1fc0cf2e3b26a24462bc930e
irakli adamashvili <adamashvili.irakli8@gmail.com>
2024-06-19 22:01:29 +0400

    cartsection

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
-export class CartComponent {
+export class CartComponent implements OnInit{
+  constructor(private service: ApiService){}
+  ngOnInit(): void {
+    this.getCartList()
+  }
 
+  public cartList: any
+
+  getCartList() {
+    this.service.getCartItems().subscribe(data => {
+      console.log(data);
+      this.cartList = data
+    })
+  }
 }
